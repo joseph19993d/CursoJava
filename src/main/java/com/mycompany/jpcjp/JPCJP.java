1da4a117b6311f96a0c03655f470a0727c195cde
joseph19993d <rodelosuaresj@gmail.com>
2023-05-01 12:15:28 -0500

    Main-start

---
+++ src/main/java/com/mycompany/jpcjp/JPCJP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jpcjp;
+
+/**
+ *
+ * @author JOSEPH
+ */
+public class JPCJP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
